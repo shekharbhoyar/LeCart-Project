01391622448580f0cc3776b3afe2cfee05aa89ec
chandrashekhar <shekharbhoyar10@gmail.com>
2025-03-11 14:20:05 +0530

    route fixe

--- frontend/src/pages/shoping-view/Checkout.jsx
+++ frontend/src/pages/shoping-view/Checkout.jsx
@@ -82,7 +82,7 @@ function ShoppingCheckout() {
     };
 
     dispatch(createNewOrder(orderData)).then((data) => {
-      console.log(data, "sangam");
+      console.log(data, "chandu");
       if (data?.payload?.success) {
         setIsPaymemntStart(true);
       } else {
