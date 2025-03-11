1a25be82e8bf57ef7b991e7b57e4cf5408137f16
chandrashekhar <shekharbhoyar10@gmail.com>
2025-03-11 14:19:52 +0530

    paypal helper configured

--- backend/helpers/paypal.js
+++ backend/helpers/paypal.js
@@ -12,3 +12,14 @@ const paypalClient = new paypal.core.PayPalHttpClient(
 );
 
 export default paypalClient;
+
+// import checkoutNodeJssdk from "@paypal/checkout-server-sdk";
+
+// const environment = new checkoutNodeJssdk.core.SandboxEnvironment(
+//   process.env.PAYPAL_CLIENT_ID,
+//   process.env.PAYPAL_SECRET
+// );
+
+// const paypalClient = new checkoutNodeJssdk.core.PayPalHttpClient(environment);
+
+// export default paypalClient;
